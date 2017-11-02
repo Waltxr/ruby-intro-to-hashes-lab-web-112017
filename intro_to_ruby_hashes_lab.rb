6a76d838c4989e740567b46baac3e18eae02279b
Adam Walter <adam.iverson.walter@gmail.com>
2017-11-02 18:23:05 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -16,7 +16,7 @@ end
 def monopoly_with_second_tier
 	monopoly = {}
 	# monopoly[:railroads] = {}
-	monopoly[:railorads] = {:pieces => 4, :names => {}, :rent_in_dollars => {} }
+	monopoly[:railorads] = {:pieces => 4, :names = {}, :rent_in_dollars = {} }
 	monopoly
 end
 
